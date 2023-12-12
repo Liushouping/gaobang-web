7df904a728e73aea19ca982da24c2643e109f157
liushouping <kose146352@gmail.com>
2023-12-12 22:30:43 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import Home from './components/Home.vue';
-import { Head } from '@vueuse/head';
+// import { Head } from '@vueuse/head';
 </script>
 <template>
-  <Head>
+<!--   <Head>
     <title>高邦創意 GAOBANG</title>
     <meta name="description" content="高邦創意專注企劃策展、主題活動，擅長結合動漫IP規劃，發揮IP魅力與主題活動的跨界結合，打造加倍的快樂魅力。">  
     <meta property="og:title" content="高邦創意 GAOBANG">
@@ -15,7 +15,7 @@ import { Head } from '@vueuse/head';
     <meta name="twitter:title" content="高邦創意 GAOBANG">
     <meta name="twitter:description" content="高邦創意專注企劃策展、主題活動，擅長結合動漫IP規劃，發揮IP魅力與主題活動的跨界結合，打造加倍的快樂魅力。">
     <meta name="twitter:image" content="https://gaobang.art/og-gaobang.png">
-  </Head>
+  </Head> -->
   <Home />
 </template>
 
