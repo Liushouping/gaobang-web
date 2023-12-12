41cac922ed4d4b545e1bc0ba313e57e65a310099
liushouping <kose146352@gmail.com>
2023-12-12 11:06:19 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,10 @@ import { Head } from '@vueuse/head';
     <meta property="og:url" content="https://gaobang.art">
     <meta property="og:site_name" content="高邦創意 GAOBANG">
     <meta property="og:type" content="website">
-    <meta property="og:image" content="https://gaobang.art/og-gaobang.png">
+    <meta property="og:image" content="/og-gaobang.png">
     <meta name="twitter:title" content="高邦創意 GAOBANG">
     <meta name="twitter:description" content="高邦創意專注企劃策展、主題活動，擅長結合動漫IP規劃，發揮IP魅力與主題活動的跨界結合，打造加倍的快樂魅力。">
-    <meta name="twitter:image" content="https://gaobang.art/og-gaobang.png">
+    <meta name="twitter:image" content="/og-gaobang.png">
   </Head>
   <Home />
 </template>
